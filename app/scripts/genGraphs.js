b9e1baac9fbe8a5b7027161e39ea45205b877183
gobyrne <obyre.gray@gmail.com>
2015-02-21 14:23:26 -0500

    Got axes showing the months properly. Graph should now be good to go.

--- app/scripts/genGraphs.js
+++ app/scripts/genGraphs.js
@@ -2,7 +2,6 @@ google.setOnLoadCallback(drawChart);
 
 function drawChart() {
   var flu_data = [];
-  flu_data.month = ['August', 'September', 'September', 'September', 'September', 'October', 'October', 'October', 'October', 'October', 'November', 'November', 'November', 'November', 'December', 'December', 'December', 'December', 'February', 'February', 'February', 'February', 'February', 'March', 'March', 'March', 'March', 'April', 'April', 'April', 'April', 'May', 'May', 'May', 'May', 'May', 'March', 'March', 'March', 'March', 'June', 'June', 'June', 'June', 'July', 'July', 'July', 'July', 'July', 'August', 'August', 'August'];
   flu_data.week = [35, 36, 37, 38, 39, 49, 49, 49, 49, 44, 45, 46, 47, 48, 49, 50, 51, 52, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34];
 
   flu_data.hospital = [0, 0, 0, 0, 3, 2, 2, 1, 2, 5.5, 6.5, 6.5, 12.5, 31, 44, 66.5, 113, 163.5, 181, 129, 118.5, 109, 108.5, 102.5, 102.5, 121.5, 121.5, 149, 152, 121.5, 94.5, 60.5, 73.5, 42.5, 36, 32.5, 22.5, 18, 8, 3.5, 4, 1, 1, 0, 0, 1.5, 1.5, 0, 0, 0, 0, 1];
@@ -38,8 +37,10 @@ function drawChart() {
   data.addRows(splitData);
 
   var options = {
-	  title: 'Deaths from influenza in 2010-2011 by this week',
-	  hAxis: {},
+	  title: 'Reported deaths in Canada from influenza in 2010-2011 by this week',
+	  hAxis: {
+		  ticks: [{v: 3, f: 'September'},{v: 12, f: 'November'},{v: 20, f: 'January'},{v: 29, f: 'March'},{v: 38, f: 'May'},{v: 46, f: 'July'}]
+	  },
 	  vAxis: {
 		  minValue: 0
 	  },
@@ -48,7 +49,10 @@ function drawChart() {
 	  },
 	  colors: ['yellow', 'red']
   };
-
+	/*
+  var dataView = new google.visualization.DataView(data);
+  dataView.setColumns([{calc: function(data, row) { return ''; }, type:'string'}, 2, 1,0]);
+  */
   var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
   chart.draw(data, options);
 }
