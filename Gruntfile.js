7006156290e5fdada782bacd6157c39fba9d412a
Matthew Russell <matthew.g.russell@gmail.com>
2015-02-21 10:23:30 -0500

    The build now seems to work

--- Gruntfile.js
+++ Gruntfile.js
@@ -364,6 +364,17 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.registerTask('noserve', 'Preview your app', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'wiredep',
+      'autoprefixer',
+    ]);
+  });
 
   grunt.registerTask('serve', 'start the server and preview your app, --allow-remote for remote access', function (target) {
     if (grunt.option('allow-remote')) {
