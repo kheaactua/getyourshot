07bdbcbf32a6897b2d5d7b2f472a0625a59f6ba2
Matthew Russell <matthew.g.russell@gmail.com>
2015-02-21 18:42:07 -0500

    Put an actual date to countdown to, fixed Gray's sofar/left that I broke

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,7 +1,11 @@
 $( document ).ready(function() {
 
 	// Countdown clock
-	var clock = $('#countdown div.clock').FlipClock(3600 * 24 * 3, {
+	var now = new Date;
+	//var flu_start = Date.now((now.getYear() -1) + "-11-01T00:00:00");
+	var flu_end = new Date((now.getYear()+1900) + "-05-01T00:00:00");
+	var seconds_diff = (flu_end - now)/1000;
+	var clock = $('#countdown div.clock').FlipClock(seconds_diff, {
 		clockFace: 'DailyCounter',
 		countdown: true,
 		showSeconds: false,
