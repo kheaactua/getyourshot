1cbb2b48829ddebb5478e7d54469bf7fcf86f8a9
Matthew Russell <matthew.g.russell@gmail.com>
2015-02-21 00:41:26 -0500

    Trying to make it pretty

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+
+	// Countdown clock
+	var clock = $('#countdown div.clock').FlipClock(3600 * 24 * 3, {
+		clockFace: 'DailyCounter',
+		countdown: true
+	});
+
+});
